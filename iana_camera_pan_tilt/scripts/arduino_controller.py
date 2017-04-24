c97ae0e0bc8e3edec745e6b8ca5077d5b9be8015
Christian Scheller <ch.scheller@hotmail.com>
2017-04-24 12:30:38 +0200

    Initial commit of camera_pan_tilt and collision_detector nodes

---
+++ iana_camera_pan_tilt/scripts/arduino_controller.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# license removed for brevity
+import serial
+import struct
+import rospy
+from iana_camera_pan_tilt.msg import PanTilt
+
+
+class ArduinoController(object):
+    def __init__(self):
+        rospy.init_node('arduino_controller', anonymous=True)
+        rospy.Subscriber("set_pan_tilt", PanTilt, self.handle_set_pan_tilt)
+        self.pant_tilt_pub = rospy.Publisher('pan_tilt', PanTilt, queue_size=10)
+        self.serial = serial.Serial('/dev/ttyACM0', 9600)
+        self.state = [90, 90]
+
+    def run(self):
+        self.pant_tilt_pub.publish(self.state[0], self.state[1])
+        rospy.spin()
+        #rate = rospy.Rate(10)  # 10hz
+        #while not rospy.is_shutdown():
+        #    rate.sleep()
+
+    def handle_set_pan_tilt(self, pan_tilt: PanTilt) -> None:
+        self.__set_pan_tilt(pan_tilt.pan, pan_tilt.tilt)
+
+    def __set_pan_tilt(self, pan: int, tilt: int) -> None:
+        pan = min(180, max(0, pan))
+        tilt = min(180, max(0, tilt))
+        state = [pan, tilt]
+        self.serial.write(bytearray(struct.pack('%sI' % len(state), *state)))
+        self.pant_tilt_pub.publish(pan, tilt)
+
+
+if __name__ == '__main__':
+    try:
+        arduino_controller = ArduinoController()
+        arduino_controller.run()
+    except rospy.ROSInterruptException:
+        pass
---
+++ iana_collision_detector/src/LookAhead.cpp
@@ -0,0 +1,53 @@
+#include <vector>
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "std_msgs/Float32.h"
+
+namespace Iana
+{
+    class LookAhead
+    {
+        ros::NodeHandle m_nodeHandle;
+        ros::Publisher m_collisionAheadPublisher;
+        ros::Subscriber m_depthImageSubscriber;
+    public:
+        LookAhead()
+        {
+            m_collisionAheadPublisher = m_nodeHandle.advertise<std_msgs::Float32>("collision_ahead", 1000);
+            m_depthImageSubscriber = m_nodeHandle.subscribe("/depth_image", 1000, &LookAhead::depthImageCallback, this);
+        }
+
+
+        void depthImageCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+        {
+            std::vector<float>::const_iterator it = msg->ranges.begin();
+            std::vector<float>::const_iterator end = msg->ranges.end();
+            float min_range = msg->range_max;
+            for (it; it != end; ++it)
+            {
+                float range = *it;
+                if (range < min_range && range >= msg->range_min)
+                {
+                    min_range = range;
+                }
+            }
+            if (min_range != msg->range_max)
+            {
+                std_msgs::Float32 minRangeMsg;
+                minRangeMsg.data = min_range;
+                m_collisionAheadPublisher.publish(minRangeMsg);
+            }
+        }
+    };
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "look_ahead");
+
+    Iana::LookAhead lookAhead();
+
+    ros::spin();
+
+    return 0;
+}
