7ce9b9a7d44be48f1d8d9e062f3a8aba486c56ef
Benjamin Meyer <benjamin.meyer@students.fhnw.ch>
2017-04-27 09:55:07 +0200

    Changed DriverRotateTest to LeftXTimes which makes use of the new DriverPlan class

--- iana_driver/src/LeftXTimes.cpp
+++ iana_driver/src/LeftXTimes.cpp
@@ -3,15 +3,15 @@
 #include "nav_msgs/Odometry.h"
 #include "../include/iana_driver/DriverPlan.h"
 #include "../include/iana_driver/VelocityChanger.h"
-#include <memory.h>
 #include <math.h>
-#include <queue>
 
 using namespace Iana;
 
+int numOfRuns = 5;
+
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "driver_plan");
+    ros::init(argc, argv, "left_x_times");
     ros::NodeHandle n;
 
     std::shared_ptr<VelocityChanger> velocityChanger = std::make_shared<VelocityChanger>(
@@ -19,19 +19,16 @@ int main(int argc, char **argv)
     );
 
     std::queue<std::shared_ptr<PlanAction>> actions;
-    actions.push(std::make_shared<DrivingForwardAction>(0)); // needed for initializing next action correctly. TODO get ride of this :)
-    actions.push(std::make_shared<DrivingForwardAction>(0.4));
-    actions.push(std::make_shared<TurningAction>(M_PI, TurningAction::Left));
+    actions.push(std::make_shared<DrivingForwardAction>(0));
+    actions.push(std::make_shared<TurningAction>(2*M_PI * numOfRuns, TurningAction::Left));
 
     DriverPlan driverPlan(
-        velocityChanger,
-        actions
+            velocityChanger,
+            actions
     );
 
     ros::Subscriber sub = n.subscribe<nav_msgs::Odometry>("/odom", 1000, &DriverPlan::Update, &driverPlan);
 
     ros::spin();
-
-    return 0;
 
 }
