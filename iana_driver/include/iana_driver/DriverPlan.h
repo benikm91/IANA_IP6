ae6a135449389594cbd6aacb765cb163cda5908f
Benjamin Meyer <benjamin.meyer@students.fhnw.ch>
2017-04-27 09:26:37 +0200

    Bugfix in TurningAction , deltaYawn was missing the 'overflow' check

--- iana_driver/include/iana_driver/DriverPlan.h
+++ iana_driver/include/iana_driver/DriverPlan.h
@@ -83,8 +83,8 @@ namespace Iana {
             double roll, pitch, yawn;
             std::tie(roll, pitch, yawn) = orientation.ToEulerianAngle();
 
-            double deltaYawn = std::abs(yawn - oldYawn);
-            if (deltaYawn > M_PI) deltaYawn = std::fmin(yawn, oldYawn) + 2*M_PI - std::fmax(yawn, oldYawn);
+            double deltaYawn = std::abs(yawn - m_oldYawn);
+            if (deltaYawn > M_PI) deltaYawn = std::fmin(yawn, m_oldYawn) + 2*M_PI - std::fmax(yawn, m_oldYawn);
             m_rotatedSoFar += deltaYawn;
             ROS_INFO_STREAM("TurningAction - Current: " << m_rotatedSoFar << " Goal: " << m_angle);
             m_oldYawn = yawn;
