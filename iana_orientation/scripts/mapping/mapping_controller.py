e57b85621f17cc1ebfadb84fa9563f63bc25de1b
Christian Scheller <ch.scheller@hotmail.com>
2017-05-11 15:56:21 +0200

    Fixed cmake build script

--- iana_orientation/scripts/mapping/mapping_controller.py
+++ iana_orientation/scripts/mapping/mapping_controller.py
@@ -5,8 +5,8 @@ import iana_orientation.srv
 class MappingController(object):
 
     def __init__(self):
-        super().__init__()
-        s = rospy.Service('/iana/orientation/set_mapping_mode', iana_orientation.srv.SetMappingMode, self.handle_set_mapping_mode)
+        super(MappingController, self).__init__()
+        self.set_mapping_mode_serviceia = rospy.Service('/iana/orientation/set_mapping_mode', iana_orientation.srv.SetMappingMode, self.handle_set_mapping_mode)
 
     def handle_set_mapping_mode(self,req):
         # TODO: implement!
