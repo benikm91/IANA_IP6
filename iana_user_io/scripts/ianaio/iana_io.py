e8f3a86aea9ad1b9803295d1120a8dce3f015597
Benjamin Meyer <benjamin.meyer@students.fhnw.ch>
2017-06-19 16:13:50 +0200

    revert old missing stuff

--- iana_user_io/scripts/ianaio/iana_io.py
+++ iana_user_io/scripts/ianaio/iana_io.py
@@ -1,21 +1,33 @@
 import rospy
-from std_msgs.msg import String
-
+from std_msgs.msg import Time
+from geometry_msgs.msg import PoseStamped
+from iana_controller.msg import Explore, GoTo
 
 class IanaTalker(object):
 
     def __init__(self):
-        self.explore_pub = rospy.Publisher('explorer', String, queue_size=10)
-        self.goto_pub = rospy.Publisher('goto', String, queue_size=10)
+        self.explore_pub = rospy.Publisher('/iana/user_command/explore', Explore, queue_size=10)
+        self.goto_pub = rospy.Publisher('/iana/user_command/go_to', GoTo, queue_size=10)
 
     def start(self):
         pass
 
     def explore(self):
-        self.explore_pub.publish("EXPLORE")
+        time = Time()
+        time.data = rospy.Time.now() + rospy.Duration(5 * 60)
+        self.explore_pub.publish(time)
 
     def goto(self, x, y):
-        self.goto_pub.publish("GOTO")
+        target_pose = PoseStamped()
+        target_pose.header.frame_id = 'map'
+        target_pose.pose.position.x = float(x)
+        target_pose.pose.position.y = float(y)
+        target_pose.pose.position.z = 0.0
+        target_pose.pose.orientation.x = float(0)
+        target_pose.pose.orientation.y = float(0)
+        target_pose.pose.orientation.z = float(1)
+        target_pose.pose.orientation.w = float(0)
+        self.goto_pub.publish(target_pose)
 
 
 class IanaIO(object):
