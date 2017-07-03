452441b089db48f4a3a86a0237aaf0c56830193f
Benjamin Meyer <benjamin.meyer@students.fhnw.ch>
2017-07-03 14:30:25 +0200

    Make param mandatory

--- debug_maps_saver/src/map_saver.py
+++ debug_maps_saver/src/map_saver.py
@@ -5,10 +5,10 @@ import rospy
 from nav_msgs.msg import OccupancyGrid
 
 counter = 0
-
+base_file_name = ""
 
 def save_map(msg):
-    base_file_name = rospy.get_param('~file', '~/map/map')
+    global base_file_name
     global counter
     file_path = "{0}_{1}".format(base_file_name, counter)
     command = "rosrun map_server map_saver -f {0}".format(file_path)
@@ -18,6 +18,7 @@ def save_map(msg):
 if __name__ == '__main__':
     try:
         rospy.init_node('maps_saver')
+	base_file_name = rospy.get_param('~file')
         rospy.Subscriber("/map", OccupancyGrid, save_map)
         rospy.spin()
     except rospy.ROSInterruptException:
