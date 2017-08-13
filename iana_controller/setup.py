aa3cd3cc40ec02e11cbb30077c2e49cf22f95c56
Christian Scheller <ch.scheller@hotmail.com>
2017-08-13 12:57:55 +0200

    Removed old setup packages (now removed)

--- iana_controller/setup.py
+++ iana_controller/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['controller', 'task', 'task.tasks', 'interaction', 'interaction.behavior'],
+    packages=['controller', 'task', 'task.tasks'],
     package_dir={'': 'scripts'},
 )
 
