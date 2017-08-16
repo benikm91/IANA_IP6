d21d1de77cb00303fe9161ee81468c1ca812af1a
Benjamin Meyer <benjamin.meyer@students.fhnw.ch>
2017-08-16 15:57:57 +0200

    Bugfix: Published to wrong driver.

--- iana_controller/scripts/task/tasks/explore_random.py
+++ iana_controller/scripts/task/tasks/explore_random.py
@@ -11,8 +11,8 @@ class ExploreRandomTask(Task):
 
     def __init__(self, msg):
         super(ExploreRandomTask, self).__init__()
-        self.driver_explore_enable_publisher = rospy.Publisher('/iana/driver_explore/enable', std_msgs.msg.Empty, queue_size=10)
-        self.driver_explore_disable_publisher = rospy.Publisher('/iana/driver_explore/disable', std_msgs.msg.Empty, queue_size=10)
+        self.driver_random_enable_publisher = rospy.Publisher('/iana/driver_random/enable', std_msgs.msg.Empty, queue_size=10)
+        self.driver_random_disable_publisher = rospy.Publisher('/iana/driver_random/disable', std_msgs.msg.Empty, queue_size=10)
         self.running = threading.Event()
         self.until = msg.until
 
@@ -27,7 +27,7 @@ class ExploreRandomTask(Task):
     def on_start(self):
         rospy.loginfo('start random exploring')
         self.running.set()
-        self.driver_explore_enable_publisher.publish()
+        self.driver_random_enable_publisher.publish()
 
     def on_resume(self):
         rospy.loginfo('resume random exploring')
@@ -36,12 +36,12 @@ class ExploreRandomTask(Task):
     def on_interrupt(self):
         rospy.loginfo('interrupt random exploring')
         self.running.clear()
-        self.driver_explore_disable_publisher.publish()
+        self.driver_random_disable_publisher.publish()
 
     def on_shutdown(self):
         rospy.loginfo('shutdown random exploring')
         self.running.clear()
-        self.driver_explore_disable_publisher.publish()
+        self.driver_random_disable_publisher.publish()
         self.terminated.set()
 
     def interruptable_by(self, task):
