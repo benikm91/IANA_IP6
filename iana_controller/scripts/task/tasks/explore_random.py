15771262b03fc89bb2ef5fdc6b6417ac2814a1d6
Benjamin Meyer <benjamin.meyer@students.fhnw.ch>
2017-08-16 15:53:44 +0200

    Bugfix + Log fix (missing in last commit)

--- iana_controller/scripts/task/tasks/explore_random.py
+++ iana_controller/scripts/task/tasks/explore_random.py
@@ -12,7 +12,7 @@ class ExploreRandomTask(Task):
     def __init__(self, msg):
         super(ExploreRandomTask, self).__init__()
         self.driver_explore_enable_publisher = rospy.Publisher('/iana/driver_explore/enable', std_msgs.msg.Empty, queue_size=10)
-        self.driver_explore_disable_publisher = rospy.Publisher('/iana/driver_explore/disable', std_msgs.msg.Empty, queue_size=10)# explore
+        self.driver_explore_disable_publisher = rospy.Publisher('/iana/driver_explore/disable', std_msgs.msg.Empty, queue_size=10)
         self.running = threading.Event()
         self.until = msg.until
 
@@ -22,33 +22,27 @@ class ExploreRandomTask(Task):
 
     def update(self, elapsed):
         if time.time() > self.until:
-            self.driver_explore_enable_publisher.publish()
+            self.terminated.set()
 
     def on_start(self):
-        rospy.logerr('start random exploring')
+        rospy.loginfo('start random exploring')
         self.running.set()
         self.driver_explore_enable_publisher.publish()
 
     def on_resume(self):
-        rospy.logerr('resume random exploring')
+        rospy.loginfo('resume random exploring')
         self.on_start()
 
     def on_interrupt(self):
-        rospy.logerr('interrupt random exploring')
+        rospy.loginfo('interrupt random exploring')
         self.running.clear()
         self.driver_explore_disable_publisher.publish()
 
     def on_shutdown(self):
-        rospy.logerr('shutdown random exploring')
+        rospy.loginfo('shutdown random exploring')
         self.running.clear()
         self.driver_explore_disable_publisher.publish()
         self.terminated.set()
 
     def interruptable_by(self, task):
         return True
-
-    def _goal_reached_callback(self, state, result):
-        if self.running.is_set():
-            rospy.logerr('exploring goal reached: terminated set!')
-            self.terminated.set()
-
