8b97bb339ed778d026e3314441fdecc2985fd4f0
Christian Scheller <ch.scheller@hotmail.com>
2017-08-16 12:15:05 +0200

    in_waiting is property, not a function

--- iana_camera_pan_tilt/scripts/arduino_controller.py
+++ iana_camera_pan_tilt/scripts/arduino_controller.py
@@ -39,7 +39,7 @@ class ArduinoController(object):
         while not success and (time.time() - start_time) < self.write_timeout:
             self.serial.timeout = self.write_timeout
             self.serial.write(bytearray(struct.pack('I' * len(self.state), *self.state)))
-            if self.serial.in_waiting() <= 1:
+            if self.serial.in_waiting <= 1:
                 self.serial.timeout = self.read_timeout
                 result = self.serial.read(1)
                 success = len(result) == 1 and result[0] == 0
