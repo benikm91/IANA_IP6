3573c63e4935088a21f69adb92d79dbdc3dbf89a
Christian Scheller <ch.scheller@hotmail.com>
2017-08-16 12:50:43 +0200

    handle arduino port connection error nicely

--- iana_camera_pan_tilt/scripts/arduino_controller.py
+++ iana_camera_pan_tilt/scripts/arduino_controller.py
@@ -14,15 +14,23 @@ class ArduinoController(object):
         self.initial_tilt = rospy.get_param("initial_tilt", 90)
         self.serial_port = rospy.get_param("serial_port", '/dev/ttyACM0')
         self.serial_baudrate = rospy.get_param("serial_baudrate", 9600)
+        self.init_timeout = rospy.get_param("init_timeout", 5)
         self.write_timeout = rospy.get_param("write_timeout", 3)
         self.read_timeout = rospy.get_param("read_timeout", 1)
         self.state = [self.initial_pan, self.initial_tilt]
 
-        rospy.Subscriber("/iana/camera/set_pan_tilt", PanTilt, self.handle_set_pan_tilt, queue_size=5)
+        success = False
+        start_time = time.time()
+        while not success and (time.time() - start_time) < self.init_timeout:
+            try:
+                self.serial = serial.Serial(self.serial_port, baudrate=self.serial_baudrate)
+            except serial.serialutil.SerialException:
+                pass
+        if not success:
+            raise Exception("Failed to connect to arduino on port '{}'".format(self.serial_port))
 
+        self.pant_tilt_sub = rospy.Subscriber("/iana/camera/set_pan_tilt", PanTilt, self.handle_set_pan_tilt, queue_size=5)
         self.pant_tilt_pub = rospy.Publisher('/iana/camera/pan_tilt', PanTilt, queue_size=10)
-
-        self.serial = serial.Serial(self.serial_port, baudrate=self.serial_baudrate)
 
     def run(self):
         self.__set_pan_tilt(self.state[0], self.state[1])
